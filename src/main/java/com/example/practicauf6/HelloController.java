b9f102db20af77d1464eeac71980da749089b6ca
Augusto <augusto.malaga.t@gmail.com>
2022-05-09 11:21:25 +0200

    Nueva interfaz

--- src/main/java/com/example/practicauf6/HelloController.java
+++ src/main/java/com/example/practicauf6/HelloController.java
@@ -2,11 +2,14 @@ package com.example.practicauf6;
 
 import Utilities.Actions;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button connect;
 
     @FXML
     protected void onHelloButtonClick() {
