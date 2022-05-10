da98df8b79872e3c23c148673458e4a815373845
Augusto <augusto.malaga.t@gmail.com>
2022-05-10 23:16:50 +0200

    vuelta atras

--- src/main/java/com/example/practicauf6/HelloController.java
+++ src/main/java/com/example/practicauf6/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
     @FXML
     private Label welcomeText;
     @FXML
-    private Button showData;
+    private Button connect;
     @FXML
 
     private ComboBox comboSchema = new ComboBox();
@@ -23,28 +23,35 @@ public class HelloController {
 
     @FXML
     protected void initialize(){
-
-        try {
-            setComboSchemas();
-        } catch (Exception e) {
-            e.printStackTrace();
+        for(String s : Actions.databases){
+            System.out.println(s);
+            comboSchema.getItems().add(s);
         }
+//        System.out.println(schemas.getValue());
+    }
+    @FXML
+    protected void onHelloButtonClick() {
+        connect.setDisable(true);
     }
     protected void setComboSchemas() throws Exception {
         for(String s : Actions.databases){
             comboSchema.getItems().add(s);
+
         }
     }
     @FXML
     protected void schemaSelected() {
         if(comboSchema.getValue() != null) {
+            System.out.println(comboSchema.getValue());
             comboTables.setDisable(false);
             Actions.setTables(comboSchema.getValue());
-            try {
-                setComboTables();
-            } catch (Exception e) {
-                e.printStackTrace();
+            System.out.println(Actions.tables.size());
+            for(String s : Actions.tables){
+                System.out.println(s);
+                System.out.println(comboSchema.getValue());
+                comboTables.getItems().add(s);
             }
+            System.out.println(comboSchema.getValue());
 
         }
     }
@@ -55,11 +62,12 @@ public class HelloController {
         }
     }
 
+
     protected void setComboTables() throws Exception {
-        for(String s : Actions.tables){
-            comboTables.getItems().add(s);
+        for(String s : Actions.databases){
+            System.out.println(s);
+            comboSchema.getItems().add(s);
         }
-        tableSelected();
     }
 
 }
